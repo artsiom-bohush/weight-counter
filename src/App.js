7144310f58017371a49d62f2c505c32a4cf61cef
Artsiom Bohush <artyom.bogush@gmail.com>
2023-01-04 18:04:18 +0100

    add Table component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import { Form } from "./components/form/Form";
 import { Header } from "./components/header/Header";
+import { Table } from "./components/table/Table";
 
 export const App = () => {
   return (
     <div className="App">
       <Header />
       <Form />
+      <Table/>
     </div>
   );
 };

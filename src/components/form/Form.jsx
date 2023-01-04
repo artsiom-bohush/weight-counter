2c938a8bbd9e4ec162f1f3382171fe75ac04bd80
Artsiom Bohush <artyom.bogush@gmail.com>
2023-01-04 17:49:24 +0100

    add form and header components

---
+++ src/components/form/Form.jsx
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+export const Form=() => {
+    return (
+      <div className="FormContainer">
+        <input placeholder="Weight" />
+        <input placeholder="Height" />
+        <button>Calculate</button>
+      </div>
+    );
+  }
+  
